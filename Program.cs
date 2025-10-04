e6bbda8fe05ff2c4a8ee10052d9008198a5e4a38
dalfonsop <dalfonsop94@gmail.com>
2025-10-04 16:28:10 -0500

    Add port to render.com

--- Program.cs
+++ Program.cs
@@ -34,6 +34,10 @@ builder.Services.AddScoped<IWhatsAppWebHookService, WhatsAppWebHookService>();
 
 var app = builder.Build();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://0.0.0.0:{port}");
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
